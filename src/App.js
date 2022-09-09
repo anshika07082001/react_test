253ed401d4a939b771d5a94e4b5928ab4da7c556
anshikachaurasiya-cedcoss <anshikachaurasiya@cedcoss.com>
2022-09-09 15:48:10 +0530

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export class App extends Component {
+  
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       val:'',
+       color:'light',
+       theme:'light'
+    }
+  }
+  clickHandler=(event)=>{
+    this.state.val=event.target.value
+    if(this.state.val==='light'){
+      this.setState({
+        color:'dark'
+      })     
+      this.setState({
+        theme:'dark'
+      })
+    }
+    if(this.state.val==='dark'){
+      this.setState({
+        color:'light'
+      })     
+      this.setState({
+        theme:'light'
+      })
+    }
+  }
+  render() {
+    return (
+      <div className='App' id={this.state.color}>
+        <button onClick={this.clickHandler} value={this.state.theme}>{this.state.theme}</button>
+        <h1>set Theme</h1>
+        <p>loremshdasguds hdshdsd igushudsadk reate a dark mode with following points
+Default white background with black text
+on change black background with white text
+Can use a single button for same whose text will change to "Dark Mode" and "Light Mode" accordingly</p>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
